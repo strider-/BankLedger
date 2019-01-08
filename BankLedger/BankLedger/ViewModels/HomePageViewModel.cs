9c7f76e213de6eafbd564d0a81dbf57f293c1df1
Michael Tighe <michaelt@zumasys.com>
2019-01-08 14:35:49 -0800

    Add HomePageViewModel

---
+++ BankLedger/BankLedger/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using BankLedger.Data;
+using BankLedger.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BankLedger.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+        public ObservableCollection<Account> Items { get; set; } = new ObservableCollection<Account>();
+
+        public Command LoadAccountsCommand { get; set; }
+
+        private IDatabaseQuery<IEnumerable<Account>> Query { get; } = new AccountWithCurrentBalanceQuery();
+
+        public HomePageViewModel()
+        {
+            Title = "Home";
+            LoadAccountsCommand = new Command(async () => await LoadData(LoadAccountsAsync));
+        }
+
+        private async Task LoadAccountsAsync()
+        {
+            Items.Clear();
+            var accounts = await Database.ExecuteAsync(Query);
+            foreach(var account in accounts)
+            {
+                Items.Add(account);
+            }
+        }
+    }
+}
--- BankLedger/BankLedger/Views/HomePage.xaml.cs
+++ BankLedger/BankLedger/Views/HomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using BankLedger.Models;
+using BankLedger.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace BankLedger.Views
@@ -6,11 +8,36 @@ namespace BankLedger.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
-        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+        private MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+
+        private HomePageViewModel _viewModel;
 
         public HomePage()
         {
             InitializeComponent();
+
+            BindingContext = _viewModel = new HomePageViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_viewModel.Items.Count == 0)
+            {
+                _viewModel.LoadAccountsCommand.Execute(null);
+            }
+        }
+
+        private async void OnAccountSelectedAsync(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is Account account)
+            {
+                var viewModel = new AccountViewModel(account);
+                var page = new AccountPage(viewModel);
+
+                await RootPage.NavigateToAsync(page);
+            }
         }
     }
 }
