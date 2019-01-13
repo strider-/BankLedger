8002df0e251be4fcf326e4e7087592a2361af6b2
Michael Tighe <strideriidx@gmail.com>
2019-01-12 17:44:31 -0800

    Set background color for transaction based on whether or not it was a recurring transaction

---
+++ BankLedger.Core/Converters/TransactionColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using BankLedger.Core.Models;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BankLedger.Core.Converters
+{
+    public class TransactionColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Transaction trans && trans.RecurringTransactionId.HasValue)
+            {
+                return Color.FromHex("#F1E8EC");
+            }
+
+            return Color.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
